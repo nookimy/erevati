7435005ccea5023a32ff6ddfdcfc55d7f619a8a2
Alina <alina@onmaster.ru>
2019-10-29 16:52:07 +0300

    Правильная верстка формы логина

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp      = require('gulp'), // Подключаем Gulp
     browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('less', function(){ // Создаем таск less
-    return gulp.src('app/less/**/*.less') // Берем источник
+    return gulp.src('app/less/style.less') // Берем источник
         .pipe(less()) // Преобразуем less в CSS посредством gulp-less
         .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
